8f9ed3a2547dce564814fcb6b74d0790c55e8ffe
brus46 <brus46@ad5ef8f6-c825-11dd-a880-a1081c7ac358>
2008-12-13 08:23:40 +0000

    Now buttons works. New buttons for the menu... in kde style boys! Search bar has the same color of buttons. Search bar now correct size if bottom.

--- trunk/src/AGM/AGM_menu_button.py
+++ trunk/src/AGM/AGM_menu_button.py
@@ -40,32 +40,53 @@ class AGM_menu_button(gtk.EventBox):
         self.icon=gtk.Image()
         self.icon.set_from_pixbuf(element["icon"])
         self.label=gtk.Label(element["name"])
-        self.action=gtk.Label()
+        
+        self.label.set_size_request(150, -1)
+        
+        self.menu_list=[]
+        if (element.has_key("other_options")):
+            for el in element["other_options"]:
+                if el.has_key("icon"):
+                    self.menu_list.append([el["name"], el["command"], el["icon"]])
+                else: self.menu_list.append([el["name"], el["command"], None])
         
         self.button1=gtk.Button()
         self.button2=gtk.Button()
         self.button3=gtk.Button()
         self.button4=gtk.Button()
         
-        self.button1.set_size_request(self.button_size, self.button_size)
-        self.button1.connect("button_press_event", self.clicked, 1)
-        self.button1.connect("enter_notify_event", self.activate_event)
-        self.button2.set_size_request(self.button_size, self.button_size)
-        self.button2.connect("button_press_event", self.clicked, 2)
-        self.button2.connect("enter_notify_event", self.activate_event)
-        self.button3.set_size_request(self.button_size, self.button_size)
-        self.button3.connect("button_press_event", self.clicked, 3)
-        self.button3.connect("enter_notify_event", self.activate_event)
-        self.button4.set_size_request(self.button_size, self.button_size)
-        self.button4.connect("button_press_event", self.clicked, 4)
-        self.button4.connect("enter_notify_event", self.activate_event)
-        
         #Button positionating
-        self.layoutL.pack_start(self.button1, False)
-        self.layoutL.pack_end(self.button3, False)
+        if len(self.menu_list)>=1:
+            self.button1.set_tooltip_text(self.menu_list[0][0])
+            self.layoutL.pack_start(self.button1, False)
+            self.button1.set_size_request(self.button_size, self.button_size)
+            self.button1.connect("button_press_event", self.clicked, self.menu_list[0][1])
+            self.button1.connect("enter_notify_event", self.activate_event, self.menu_list[0][0])
+        if len(self.menu_list)>=3:
+            self.button3.set_tooltip_text(self.menu_list[2][0])
+            self.layoutL.pack_end(self.button3, False)
+            self.button3.set_size_request(self.button_size, self.button_size)
+            self.button3.connect("button_press_event", self.clicked, self.menu_list[2][1])
+            self.button3.connect("enter_notify_event", self.activate_event, self.menu_list[2][0])
+        LSpacing=gtk.Label()
+        LSpacing.set_size_request(self.button_size, -1)
+        self.layoutL.pack_start(LSpacing)
         
-        self.layoutR.pack_start(self.button2, False)
-        self.layoutR.pack_end(self.button4, False)
+        if len(self.menu_list)>=2:
+            self.button2.set_tooltip_text(self.menu_list[1][0])
+            self.layoutR.pack_start(self.button2, False)
+            self.button2.set_size_request(self.button_size, self.button_size)
+            self.button2.connect("button_press_event", self.clicked, self.menu_list[1][1])
+            self.button2.connect("enter_notify_event", self.activate_event, self.menu_list[1][0])
+        if len(self.menu_list)>=4:
+            self.button4.set_tooltip_text(self.menu_list[3][0])
+            self.layoutR.pack_end(self.button4, False)
+            self.button4.set_size_request(self.button_size, self.button_size)
+            self.button4.connect("button_press_event", self.clicked, self.menu_list[3][1])
+            self.button4.connect("enter_notify_event", self.activate_event, self.menu_list[3][0])
+        RSpacing=gtk.Label()
+        RSpacing.set_size_request(self.button_size, -1)
+        self.layoutR.pack_start(RSpacing)
         
         self.layout.pack_start(self.icon, False)
         self.layout.pack_start(self.label, False)
@@ -79,7 +100,7 @@ class AGM_menu_button(gtk.EventBox):
         self.container.pack_end(self.layoutR, False)
         self.add(self.container)
         
-        self.connect("button_press_event", self.clicked, 0)
+        self.connect("button_press_event", self.clicked)
         
         self.connect("enter_notify_event", self.activate_event)
         self.connect("leave_notify_event", self.leave_event)
@@ -95,11 +116,13 @@ class AGM_menu_button(gtk.EventBox):
     def modify_fg(self, state, color):
         self.label.modify_fg(state, color)
     
-    def clicked(self, button, event, caller=None):
-        if caller==None or caller==0:
+    def clicked(self, button, event, command=None):
+        if command==None:
             self.ItemClicked(button, self.element["plugin"], self.element["el"]["type"], self.element["el"]["obj"])
         else: 
-            print caller
+            if (os.fork()==0):
+                os.execvp(command[0], command)
+                sys.exit(-1)
     
     def color(self, obj):
         try:
@@ -121,6 +144,7 @@ class AGM_menu_button(gtk.EventBox):
     def activate_event(self, obj, event, text=""):
         if text=="":
             self.label.set_text(self.element["el"]["name"])
+            self.icon.set_from_pixbuf(self.element["el"]["icon"])
         else:
             self.label.set_text(text)
         self.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse(conf.selectedbgcolor))

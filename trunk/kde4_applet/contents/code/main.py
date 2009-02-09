9900aaac1903ec0024288a91fcbd0c80effaa1b3
brus46 <brus46@ad5ef8f6-c825-11dd-a880-a1081c7ac358>
2009-02-09 09:11:46 +0000

    Start working on kde4 plasmoid for agm

---
+++ trunk/kde4_applet/contents/code/main.py
@@ -0,0 +1,24 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyKDE4.plasma import Plasma
+from PyKDE4 import plasmascript
+from AGM.AGM_Main_Window import AGM as agm
+import AGM.AGM_utils as utils
+ 
+class HelloPython(plasmascript.Applet):
+    def __init__(self,parent,args=None):
+        plasmascript.Applet.__init__(self,parent)
+ 
+    def init(self):
+        self.setHasConfigurationInterface(False)
+        self.resize(125, 125)
+        self.setAspectRatioMode(Plasma.Square)
+ 
+    def paintInterface(self, painter, option, rect):
+        painter.save()
+        painter.setPen(Qt.white)
+        painter.drawText(rect, Qt.AlignVCenter | Qt.AlignHCenter, "Menu")
+        painter.restore()
+ 
+def CreateApplet(parent):
+    return HelloPython(parent)
